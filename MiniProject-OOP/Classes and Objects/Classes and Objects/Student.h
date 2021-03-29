a3d389f716c8ea8ccda2e9eda67d9cd2c653bb0a
AndreeaMariaDraghici <draghici108@gmail.com>
2021-03-29 14:12:44 +0300

    Create Student.h

---
+++ MiniProject-OOP/Classes and Objects/Classes and Objects/Student.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+class Student
+{
+private:
+    int scores[5];
+public:
+    void input() {
+        for (int i = 0; i < 5; i++) {
+            cin >> scores[i];
+        }
+    }
+    int calculateTotalScore() {
+        int total = 0;
+        for (int i = 0; i < 5; i++) {
+            total += scores[i];
+        }
+        return total;
+    }
+};
+
